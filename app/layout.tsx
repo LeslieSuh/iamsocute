eb438d6163754ee7fcf32a8471872efd04211cb1
Leslie Suh <lesliejysuh@gmail.com>
2023-08-03 21:48:24 +0100

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'I Am So Cute',
+  title: 'iamsocute',
   description: 'Help people to be cute',
 }
 
